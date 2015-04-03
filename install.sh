e05e5d8de0f45a92716dd1ed0b51bad8a8b8d7dd
Son Nguyen <anh.s.nguyen@gmail.com>
2015-04-03 16:04:57 +1100

    Added gitignore file

--- install.sh
+++ install.sh
@@ -5,5 +5,9 @@ ln -sf ~/dotties/tmux.conf ~/.tmux.conf
 ln -sf ~/dotties/vimrc ~/.vimrc
 ln -sf ~/dotties/bash_profile ~/.bash_profile
 ln -sf ~/dotties/bashrc ~/.bashrc
+ln -sf ~/dotties/gitignore ~/.gitignore
+
+echo "Setting up git ignore"
+git config --global core.excludesfile ~/.gitignore
 
 echo "Sweet, all ready."
